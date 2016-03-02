bf1f4f483f169599bdda54c7f340a3e7bf757b55
Tim Roes <tim.roes@inovex.de>
2016-03-02 20:04:08 +0100

    feat: auto switch to .node-version via nvm

---
+++ conf.d/95-nvm.zsh
@@ -0,0 +1,29 @@
+check_node_version() {
+	# When a .node-version file in the current directory exists ..
+	if [ -f ".node-version" ]; then
+		nodeVersion=`cat .node-version`
+		# .. check whether the current node version is the required one
+		if [ `node -v` != "v$nodeVersion" ]; then
+			# If not use nvm to set the version to the one from .node-version
+			echo -e "$fg_bold[yellow]Found .node-version file!$reset_color Switching to node v$nodeVersion ..."
+			nvm install $nodeVersion
+			if [ $? -eq 0 ]; then
+				echo -e "$fg_bold[green]Done!$reset_color"
+			else
+				echo -e "$fg_bold[red]Failed!$reset_color"
+			fi
+		fi
+	fi
+}
+
+if [ -d "$HOME/.nvm" ]; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+	# Check whether nvm is installed successfully
+	type nvm > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		# If nvm was installed successfully setup the check for a .node-version file
+		add-zsh-hook precmd check_node_version
+	fi
+fi
