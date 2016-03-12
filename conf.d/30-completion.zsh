86f8cc43bef2db5cde7e9c0a9b677cbe284f12b4
Tim Roes <mail@timroes.de>
2016-03-12 18:46:09 +0100

    feat: improve auto completion on tab

--- conf.d/30-completion.zsh
+++ conf.d/30-completion.zsh
@@ -1,3 +1,8 @@
 # Enable navigating in completions
 zstyle ':completion:*' menu select
 zstyle ':completion::complete:*' use-cache 1
+
+# Enable case insensitive auto completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+# Automatically autocomplete to first menu entry on tab
+setopt MENU_COMPLETE
