1bd4ec7b554dac394800cc23a91ed4ed2615e9f9
Tim Roes <mail@timroes.de>
2016-04-09 22:58:12 +0200

    add delete and insert key bindings

--- conf.d/50-keys.zsh
+++ conf.d/50-keys.zsh
@@ -6,3 +6,5 @@ key[End]=${terminfo[kend]}
 
 [[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
 [[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
+[[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+[[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
