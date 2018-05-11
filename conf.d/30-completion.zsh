9f98253a1292b21d20db966e8b086265ea9a5e7b
Tim Roes <mail@timroes.de>
2018-05-11 21:17:14 +0200

    Improve cd behavior

--- conf.d/30-completion.zsh
+++ conf.d/30-completion.zsh
@@ -9,6 +9,8 @@ setopt MENU_COMPLETE
 
 # Enable autocompletion of directories and auto change without a command
 setopt autocd
+# Setup some paths that will automatically tried to be cd'ed into when completing folders
+cdpath=($HOME/elastic)
 
 
 include_autosuggest=/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
