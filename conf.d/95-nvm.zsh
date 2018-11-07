4244e7c927ff4b5c85f0030b6811a0271db7813f
Tim Roes <mail@timroes.de>
2018-11-07 09:15:39 +0100

    Improve nvm handling

--- conf.d/95-nvm.zsh
+++ conf.d/95-nvm.zsh
@@ -8,7 +8,12 @@ check_node_version() {
 		if [ `node -v` != "v$nodeVersion" ]; then
 			# If not use nvm to set the version to the one from .node-version
 			echo -e "$fg_bold[yellow]Found .node-version file!$reset_color Switching to node v$nodeVersion ..."
-			nvm install $nodeVersion
+			nvm ls $nodeVersion &> /dev/null
+			if [ $? -eq 0 ]; then
+				nvm use $nodeVersion
+			else
+				nvm install $nodeVersion
+			fi
 			if [ $? -eq 0 ]; then
 				echo -e "$fg_bold[green]Done!$reset_color"
 			else
