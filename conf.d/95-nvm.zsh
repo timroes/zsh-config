c0288cfc6bc71d8bdcdda545af3e4e2e6442adfb
Tim Roes <mail@timroes.de>
2019-02-22 09:35:06 +0100

    Improve nvm config

--- conf.d/95-nvm.zsh
+++ conf.d/95-nvm.zsh
@@ -1,13 +1,11 @@
 check_node_version() {
-	# When a .node-version file in the current directory exists ..
-	if [[ "$IGNORE_NODE_VERSION" != "1" && -f ".node-version" ]]; then
-		alias node > /dev/null && unalias node > /dev/null
-		alias npm > /dev/null && unalias npm > /dev/null
-		nodeVersion=`cat .node-version`
+	# When a .nvmrc file in the current directory exists ..
+	if [[ "$IGNORE_NODE_VERSION" != "1" && -f ".nvmrc" ]]; then
+		nodeVersion=`cat .nvmrc`
 		# .. check whether the current node version is the required one
 		if [ `node -v` != "v$nodeVersion" ]; then
 			# If not use nvm to set the version to the one from .node-version
-			echo -e "$fg_bold[yellow]Found .node-version file!$reset_color Switching to node v$nodeVersion ..."
+			echo -e "$fg_bold[yellow]Found .nvmrc file!$reset_color Switching to node v$nodeVersion ..."
 			nvm ls $nodeVersion &> /dev/null
 			if [ $? -eq 0 ]; then
 				nvm use $nodeVersion
@@ -23,17 +21,7 @@ check_node_version() {
 	fi
 }
 
-if [ -d "$HOME/.nvm" ]; then
-	export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use
-
-	# Check whether nvm is installed successfully
-	type nvm > /dev/null 2>&1
-	if [ $? -eq 0 ]; then
-		# If nvm was installed successfully setup the check for a .node-version file
-		add-zsh-hook precmd check_node_version
-
-		alias node='unalias node ; unalias npm ; nvm use default ; node $@'
-		alias npm='unalias node ; unalias npm ; nvm use default ; npm $@'
-	fi
+if [ -f "/usr/share/nvm/init-nvm.sh" ]; then
+	source /usr/share/nvm/init-nvm.sh
+	add-zsh-hook precmd check_node_version
 fi
